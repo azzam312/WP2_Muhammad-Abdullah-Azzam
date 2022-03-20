276ad660662ecbde2e750f89440d6cfabf7b25d8
azzam312 <62980433+azzam312@users.noreply.github.com>
2022-03-20 22:26:20 +0700

    first commit

--- Contoh1.php
+++ Contoh1.php
@@ -4,9 +4,9 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo"Nama Saya Muhammad Abdullah Azzam
+        echo"Nama Saya Lailatul Romadhon Bachtiar
              Saya tinggal di daerah Tangerang
              olah raga yang saya sukai adalah 
-             Basket";
+             Berenang";
     }
 }
